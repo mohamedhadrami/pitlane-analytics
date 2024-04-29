53915c2d1e16aa3c78441c43757a22c204bc392d
mohamedhadrami <hadrami.mohamed@pringlerobotics.ai>
2024-04-29 16:38:22 -0500

    added optional size prop

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,14 +3,14 @@
 import React from "react";
 import { Triangle } from "react-loader-spinner";
 
-const Loading: React.FC = () => {
+const Loading: React.FC<{ size?: number }> = ({ size }) => {
   return (
     <div className="loading-overlay flex flex-col justify-center items-center h-[calc(100vh-150px)]">
       <div className="loading-spinner">
         <Triangle
           visible={true}
-          height={75}
-          width={75}
+          height={size ? size : 75}
+          width={size ? size : 75}
           color="#e10600"
           ariaLabel="triangle-loading"
           wrapperStyle={{}}
