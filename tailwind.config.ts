cb4deca89db9b1044c46c7c74ca5af6cfb0a7750
mohamedhadrami <hadrami.mohamed@pringlerobotics.ai>
2024-04-28 20:03:29 -0500

    added shadcn UI and updates tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,19 +3,44 @@ import type { Config } from "tailwindcss";
 import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
-const config: Config = {
+const config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
+  
   darkMode: "class",
   plugins: [
+    require("tailwindcss-animate"),
     nextui({
       themes: {
         dark: {
@@ -24,11 +49,15 @@ const config: Config = {
               DEFAULT: "#E10600",
               foreground: "#FFFFFF"
             },
+            secondary: {
+              DEFAULT: "#f3e600"
+            },
             focus: "#00FF00",
           },
         },
       },
     }),
   ],
-};
-export default config;
+} satisfies Config
+
+export default config
