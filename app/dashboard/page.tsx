288da1e5ff1c42b20d565db33f9cd90a03c84375
mohamedhadrami <hadrami.mohamed@pringlerobotics.ai>
2024-05-25 11:07:07 -0500

    improved to use LiveSettingsContext and addded delays in between each api get call to meet limitations of OpenF1 API

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,38 @@
-// @/app/dashboard/page.tsx
-"use client"
+// app/dashboard/page.tsx
+"use client";
 
 import React, { useEffect, useState } from "react";
-import { DriverParams, MeetingParams, RaceControlParams, SessionParams, StintParams, TeamRadioParams, WeatherParams, LapParams, PositionParams, IntervalParams } from "../../interfaces/openF1";
-import { fetchDrivers, fetchIntervals, fetchLaps, fetchMeeting, fetchPosition, fetchRaceControl, fetchSession, fetchStint, fetchTeamRadio, fetchWeather } from "../../services/openF1Api";
-import { Grid } from "@mui/material";
+import {
+    DriverParams,
+    MeetingParams,
+    RaceControlParams,
+    SessionParams,
+    StintParams,
+    TeamRadioParams,
+    WeatherParams,
+    LapParams,
+    PositionParams,
+    IntervalParams,
+} from "../../interfaces/openF1";
+import {
+    fetchDrivers,
+    fetchIntervals,
+    fetchLaps,
+    fetchMeeting,
+    fetchPosition,
+    fetchRaceControl,
+    fetchSession,
+    fetchStint,
+    fetchTeamRadio,
+    fetchWeather,
+} from "../../services/openF1Api";
 import TopBanner from "./TopBanner";
 import LiveTiming from "./LiveTiming";
 import RaceControl from "./RaceControl";
 import TeamRadios from "./TeamRadio";
+import LiveSettings from "./LiveSettings";
+import { Divider } from "@nextui-org/react";
+import { LiveSettingsProvider, useLiveSettings } from "./LiveSettingsContext";
 
 const Dashboard: React.FC = () => {
     const [meeting, setMeeting] = useState<MeetingParams>();
@@ -22,86 +46,167 @@ const Dashboard: React.FC = () => {
     const [intervals, setIntervals] = useState<IntervalParams[]>([]);
     const [positions, setPositions] = useState<PositionParams[]>([]);
 
-    const meeting_test = "latest"; //1234;
-    const session_test = "latest"; //9507;
+    const meeting_test = "latest";//1234;
+    const session_test = "latest";//9507;
 
     const params = {
         meeting_key: meeting_test,
-        session_key: session_test
-    }
+        session_key: session_test,
+    };
 
-    const fetchMeetingData = async () => {
-        const meetingParams: MeetingParams = {meeting_key: meeting_test}
-        const data = await fetchMeeting(meetingParams);
-        setMeeting(data[0]);
-    }
-
-    const fetchSessionData = async () => {
-        const data = await fetchSession(params);
-        setSession(data[0]);
-    }
-
-    const fetchDriverData = async () => {
-        const data = await fetchDrivers(params);
-        setDrivers(data);
-    }
-
-    const fetchRaceControlData = async () => {
-        const data = await fetchRaceControl(params);
-        setRaceControl(data);
-    }
-
-    async function fetchTeamRadioData() {
-        const data = await fetchTeamRadio(params);
-        setTeamRadio(data);
-    }
+    const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
     async function fetchData() {
-        const weatherData = await fetchWeather(params);
-        setWeather(weatherData.pop());
-        const stintData = await fetchStint(params);
-        setStints(stintData);
-        const lapData = await fetchLaps(params);
-        setLaps(lapData);
-        const intervalData = await fetchIntervals(params);
-        setIntervals(intervalData);
-        const positionData = await fetchPosition(params);
-        setPositions(positionData);
+        const meetingParams: MeetingParams = { meeting_key: meeting_test };
+
+        const fetchFunctions = [
+            async () => {
+                const meetingData = await fetchMeeting(meetingParams);
+                setMeeting(meetingData[0]);
+            },
+            async () => {
+                const sessionData = await fetchSession(params);
+                setSession(sessionData[0]);
+            },
+            async () => {
+                const driverData = await fetchDrivers(params);
+                setDrivers(driverData);
+            },
+            async () => {
+                const controlData = await fetchRaceControl(params);
+                setRaceControl(controlData);
+            },
+            async () => {
+                const radioData = await fetchTeamRadio(params);
+                setTeamRadio(radioData);
+            },
+            async () => {
+                const weatherData = await fetchWeather(params);
+                setWeather(weatherData.pop());
+            },
+            async () => {
+                const stintData = await fetchStint(params);
+                setStints(stintData);
+            },
+            async () => {
+                const lapData = await fetchLaps(params);
+                setLaps(lapData);
+            },
+            async () => {
+                const intervalData = await fetchIntervals(params);
+                setIntervals(intervalData);
+            },
+            async () => {
+                const positionData = await fetchPosition(params);
+                setPositions(positionData);
+            },
+        ];
+
+        for (let i = 0; i < fetchFunctions.length; i++) {
+            await fetchFunctions[i]();
+            if (i < fetchFunctions.length - 1) {
+                await delay(350); // 350 ms delay between calls to stay within rate limit
+            }
+        }
     }
 
     useEffect(() => {
-        fetchMeetingData();
-        fetchSessionData();
-        fetchDriverData();
-        fetchRaceControlData();
-        fetchTeamRadioData();
         fetchData();
-    }, [])
-
-    const isBanner: boolean = true;
-    const isLive: boolean = true;
-    const isRace: boolean = true;
-    const isRadio: boolean = true;
+    }, []);
 
     return (
-        <>
-            <Grid container spacing={0}>
-                {meeting && session && weather && isBanner && (
-                    <Grid item xs={12}><TopBanner meeting={meeting} session={session} weather={weather} /></Grid>
+        <LiveSettingsProvider>
+            <DashboardContent
+                meeting={meeting}
+                session={session}
+                drivers={drivers}
+                raceControl={raceControl}
+                teamRadio={teamRadio}
+                weather={weather}
+                stints={stints}
+                laps={laps}
+                intervals={intervals}
+                positions={positions}
+            />
+        </LiveSettingsProvider>
+    );
+};
+
+interface DashboardContentProps {
+    meeting: MeetingParams | undefined;
+    session: SessionParams | undefined;
+    drivers: DriverParams[];
+    raceControl: RaceControlParams[];
+    teamRadio: TeamRadioParams[];
+    weather: WeatherParams | undefined;
+    stints: StintParams[];
+    laps: LapParams[];
+    intervals: IntervalParams[];
+    positions: PositionParams[];
+}
+
+const DashboardContent: React.FC<DashboardContentProps> = ({
+    meeting,
+    session,
+    drivers,
+    raceControl,
+    teamRadio,
+    weather,
+    stints,
+    laps,
+    intervals,
+    positions,
+}) => {
+    const { settings } = useLiveSettings();
+
+    const findSetting = (name: string) => settings.find(setting => setting.name === name);
+
+    const isBanner = findSetting('Show Stats Banner')?.value;
+    const isLive = findSetting('Show Live Table')?.value;
+    const isRace = findSetting('Show Race Control')?.value;
+    const isRadio = findSetting('Show Team Radio')?.value;
+
+    return (
+        <div className="mx-auto">
+            <div className="flex flex-row items-center w-full h-10">
+                {isBanner && meeting && session && weather && (
+                    <div className="flex-grow overflow-hidden my-auto">
+                        <TopBanner meeting={meeting} session={session} weather={weather} />
+                    </div>
                 )}
+                <Divider orientation="vertical" />
+                {drivers && stints && laps && positions && (
+                    <div className="mx-2 flex align-middle">
+                        <LiveSettings />
+                    </div>
+                )}
+            </div>
+            <div className="grid grid-cols-1 gap-3 lg:grid-cols-1 xl:grid-cols-2">
                 {drivers && stints && laps && positions && isLive && (
-                    <Grid item xs={12} className="m-3"><LiveTiming drivers={drivers} stints={stints} laps={laps} positions={positions} intervals={intervals} /></Grid>
+                    <div className="col-span-1 xl:col-span-1 m-3">
+                        <LiveTiming
+                            drivers={drivers}
+                            stints={stints}
+                            laps={laps}
+                            positions={positions}
+                            intervals={intervals}
+                        />
+                    </div>
                 )}
-                {raceControl && isRace && (
-                    <Grid item xs><RaceControl drivers={drivers} raceControl={raceControl} /></Grid>
-                )}
-                {teamRadio && isRadio && (
-                    <Grid item xs>
-                        <TeamRadios drivers={drivers} teamRadio={teamRadio} />
-                    </Grid>
-                )}
-            </Grid>
-        </>
+                <div className="grid grid-cols-2 gap-3 xl:grid-cols-1">
+                    {raceControl && isRace && (
+                        <div className="col-span-2 md:col-span-1">
+                            <RaceControl drivers={drivers} raceControl={raceControl} />
+                        </div>
+                    )}
+                    {teamRadio && isRadio && (
+                        <div className="col-span-2 md:col-span-1">
+                            <TeamRadios drivers={drivers} teamRadio={teamRadio} />
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
     );
 };
 
