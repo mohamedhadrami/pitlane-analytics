c3aa4d5daf70c186b39455eee924682f6266418a
mohamedhadrami <hadrami.mohamed@pringlerobotics.ai>
2024-06-05 00:13:22 -0500

    moved LiveSettingsContext to context folder

--- context/LiveSettingsContext.tsx
+++ context/LiveSettingsContext.tsx
@@ -29,6 +29,8 @@ export const LiveSettingsProvider = ({ children }: { children: ReactNode }) => {
     const [isShowTyre, setIsShowTyre] = useState(true);
     const [isShowGapToLeader, setIsShowGapToLeader] = useState(true);
     const [isShowStintNumber, setIsShowStintNumber] = useState(true);
+    const [isShowLapColumn, setIsShowLapColumn] = useState(true);
+    const [isShowSectors, setIsShowSectors] = useState(true);
 
     // RACE Control
     const [isShowRaceControlTime, setIsShowRaceControlTime] = useState(true);
@@ -37,9 +39,11 @@ export const LiveSettingsProvider = ({ children }: { children: ReactNode }) => {
 
     // TEAM Radio
     const [isShowTeamRadioTime, setIsShowTeamRadioTime] = useState(true);
-
-    // Example of a string setting
     const [fastestLapColor, setFastestLapColor] = useState('#ff0000');
+    
+    // TRACK
+    const [isShowTrack, setIsShowTrack] = useState(true);
+    
 
     const settings: LiveSetting<any>[] = [
         { category: "stats-banner", name: 'Show Stats Banner', type: 'boolean', value: isBanner, setValue: setIsBanner },
@@ -48,6 +52,8 @@ export const LiveSettingsProvider = ({ children }: { children: ReactNode }) => {
         { category: "live-table", name: 'Show Tyre', type: 'boolean', value: isShowTyre, setValue: setIsShowTyre },
         { category: "live-table", name: 'Show Gap To Leader', type: 'boolean', value: isShowGapToLeader, setValue: setIsShowGapToLeader },
         { category: "live-table", name: 'Show Stint Number', type: 'boolean', value: isShowStintNumber, setValue: setIsShowStintNumber },
+        { category: "live-table", name: 'Show Lap Column', type: 'boolean', value: isShowLapColumn, setValue: setIsShowLapColumn },
+        { category: "live-table", name: 'Show Sectors', type: 'boolean', value: isShowSectors, setValue: setIsShowSectors },
         { category: "race-control", name: 'Show Race Control', type: 'boolean', value: isRace, setValue: setIsRace },
         { category: "race-control", name: 'Show Race Control Time', type: 'boolean', value: isShowRaceControlTime, setValue: setIsShowRaceControlTime },
         { category: "race-control", name: 'Show Red Flag', type: 'boolean', value: isShowRedFlag, setValue: setIsShowRedFlag },
@@ -55,12 +61,14 @@ export const LiveSettingsProvider = ({ children }: { children: ReactNode }) => {
         { category: "team-radio", name: 'Show Team Radio', type: 'boolean', value: isRadio, setValue: setIsRadio },
         { category: "team-radio", name: 'Show Team Radio Time', type: 'boolean', value: isShowTeamRadioTime, setValue: setIsShowTeamRadioTime },
         { category: "team-radio", name: 'Fastest Lap Color', type: 'string', value: fastestLapColor, setValue: setFastestLapColor },
+        { category: "track", name: 'Show Track', type: 'boolean', value: isShowTrack, setValue: setIsShowTrack },
     ];
 
     return (
         <LiveSettingsContext.Provider value={{ settings }}>
             {children}
         </LiveSettingsContext.Provider>
+
     );
 };
 
