d3123351e1964ab800f5d518f825cf3e96b340e2
mohamedhadrami <hadrami.mohamed@pringlerobotics.ai>
2024-06-05 00:18:24 -0500

    allowing columns to be hidden from livesettings

--- components/Dashboard/LiveTiming.tsx
+++ components/Dashboard/LiveTiming.tsx
@@ -1,20 +1,14 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { Spacer, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
-import { DriverParams, IntervalParams, LapParams, PositionParams, StintParams } from "../../interfaces/openF1";
-import SectorSegment from "./SectorSegments";
-import { getCompoundComponent } from "../Tyres";
-import { formatSecondsToTime } from "../../utils/helpers";
-import Loading from "../Loading";
-import { useLiveSettings } from "./LiveSettingsContext";
+// @/components/Dashboard/LiveTiming.tsx
 
-const headers = [
-    { name: "Position", uid: "position" },
-    { name: "Gap", uid: "gap" },
-    { name: "Tyre", uid: "tyre" },
-    { name: "Lap", uid: "lap" },
-    { name: "Lap Time", uid: "lapTime" },
-    { name: "Sectors", uid: "sectors" }
-];
+import React, { useEffect, useMemo, useState } from "react";
+import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import { DriverParams, IntervalParams, LapParams, PositionParams, StintParams } from "@/interfaces/openF1";
+import SectorSegment from "./SectorSegments";
+import { getCompoundComponent } from "@/components/Tyres";
+import { formatSecondsToTime } from "@/utils/helpers";
+import Loading from "../Loading";
+import { useLiveSettings } from "../../context/LiveSettingsContext";
+import { Minus } from "lucide-react";
 
 interface LiveTimingProps {
     drivers: DriverParams[];
@@ -32,12 +26,26 @@ const LiveTiming: React.FC<LiveTimingProps> = ({ drivers, stints, laps, position
         showTyre: true,
         showGapToLeader: true,
         showStintNumber: true,
+        showLapColumn: true,
+        showSectors: true,
     };
 
     const [isShowFastestLap, setIsShowFastestLap] = useState(defaultSettings.showFastestLap);
     const [isShowTyre, setIsShowTyre] = useState(defaultSettings.showTyre);
     const [isShowGapToLeader, setIsShowGapToLeader] = useState(defaultSettings.showGapToLeader);
     const [isShowStintNumber, setIsShowStintNumber] = useState(defaultSettings.showStintNumber);
+    const [isShowLapColumn, setIsShowLapColumn] = useState(defaultSettings.showLapColumn);
+    const [isShowSectors, setIsShowSectors] = useState(defaultSettings.showSectors);
+    const [tableKey, setTableKey] = useState(0);
+
+    const headers = [
+        { name: "Position", uid: "position", visible: true },
+        { name: "Gap", uid: "gap", visible: true },
+        { name: "Tyre", uid: "tyre", visible: true },
+        { name: "Lap", uid: "lap", visible: isShowLapColumn },
+        { name: "Lap Time", uid: "lapTime", visible: true },
+        { name: "Sectors", uid: "sectors", visible: isShowSectors }
+    ];
 
     useEffect(() => {
         const findSetting = (name: string) => settings.find(setting => setting.name === name)?.value;
@@ -46,7 +54,16 @@ const LiveTiming: React.FC<LiveTimingProps> = ({ drivers, stints, laps, position
         setIsShowTyre(findSetting('Show Tyre') ?? defaultSettings.showTyre);
         setIsShowGapToLeader(findSetting('Show Gap To Leader') ?? defaultSettings.showGapToLeader);
         setIsShowStintNumber(findSetting('Show Stint Number') ?? defaultSettings.showStintNumber);
-    }, [settings, defaultSettings.showFastestLap, defaultSettings.showTyre, defaultSettings.showGapToLeader, defaultSettings.showStintNumber]);
+        setIsShowLapColumn(findSetting('Show Lap Column') ?? defaultSettings.showLapColumn);
+        setIsShowSectors(findSetting('Show Sectors') ?? defaultSettings.showSectors);
+        setTableKey(prevKey => prevKey + 1);
+    }, [ settings, 
+        defaultSettings.showFastestLap, 
+        defaultSettings.showTyre, 
+        defaultSettings.showGapToLeader, 
+        defaultSettings.showStintNumber, 
+        defaultSettings.showLapColumn, 
+        defaultSettings.showSectors ]);
 
     const [sortedDrivers, setSortedDrivers] = useState<DriverParams[]>([]);
     const [currentLap, setCurrentLap] = useState<number | undefined>(0);
@@ -77,7 +94,7 @@ const LiveTiming: React.FC<LiveTimingProps> = ({ drivers, stints, laps, position
         [],
     );
 
-    const renderCell = (driver: DriverParams, columnKey: React.Key, laps: LapParams[], stints: StintParams[]) => {
+    const renderCell = (driver: DriverParams, columnKey: React.Key, laps: LapParams[], stints: StintParams[]): React.ReactNode => {
         const lap = laps?.filter(lap => lap.driver_number === driver.driver_number).pop();
         const fastestLap = laps
             .filter(lap => lap.driver_number === driver.driver_number && lap.lap_duration !== 0)
@@ -96,15 +113,25 @@ const LiveTiming: React.FC<LiveTimingProps> = ({ drivers, stints, laps, position
                 }
                 return (
                     <div className="flex justify-center gap-1">
-                        <span className="text-center">{position}</span>
+                        <span className="text-center">{driver.driver_number}</span>
                         <span style={{ color: `#${driver?.team_colour}`, textAlign: "center" }}>{driver.name_acronym}</span>
                     </div>
                 );
             case "lapTime":
                 return (
                     <div className="flex flex-col">
-                        <div className="">{formatSecondsToTime(lap?.lap_duration)}</div>
-                        {isShowFastestLap ? (<div className="font-thin flex flex-row justify-center gap-1">{formatSecondsToTime(fastestLap?.lap_duration)} {`(${fastestLap?.lap_number})`}</div>) : ""}
+                        {lap && lap.lap_duration ? (
+                            <div className="">{formatSecondsToTime(lap?.lap_duration)}</div>
+                        ) : (
+                            <div className="mx-auto"><Minus /></div>
+                        )}
+                        {isShowFastestLap ? (fastestLap && fastestLap?.lap_duration ? (
+                            <div className="font-thin flex flex-row justify-center gap-1">
+                            {formatSecondsToTime(fastestLap?.lap_duration)} {`(${fastestLap?.lap_number})`}
+                            </div>
+                        ) : (
+                            <div className="mx-auto"><Minus /></div>
+                        )) : ""}
                     </div>
                 );
             case "gap":
@@ -144,10 +171,15 @@ const LiveTiming: React.FC<LiveTimingProps> = ({ drivers, stints, laps, position
 
     const renderRow = (driver: DriverParams) => {
         const lap = laps?.filter(lap => lap.driver_number === driver.driver_number).pop();
-        const isGreyedOut = currentLap && lap && (currentLap - lap.lap_number! >= 3);
+        let isGreyedOut = currentLap && lap && (currentLap - lap.lap_number! >= 3);
+        if (!lap) isGreyedOut = true;
         return (
-            <TableRow key={driver.driver_number} className={isGreyedOut ? "brightness-50 bg-white" : ""}>
-                {(columnKey) => <TableCell className="bg-[#111] text-center">{renderCell(driver, columnKey, laps, stints)}</TableCell>}
+            <TableRow key={driver.driver_number} className={isGreyedOut ? "brightness-[0.35]" : ""}>
+                {headers.filter(header => header.visible).map(header => (
+                    <TableCell key={header.uid} className="bg-[#111] text-center">
+                        {renderCell(driver, header.uid, laps, stints)}
+                    </TableCell>
+                ))}
             </TableRow>
         )
     }
@@ -158,11 +190,12 @@ const LiveTiming: React.FC<LiveTimingProps> = ({ drivers, stints, laps, position
                 <>
                     {laps && laps.length > 0 ? (
                         <Table
+                            key={tableKey}
                             aria-label="Live timing data"
                             classNames={classNames}
                             className="max-screen-xs"
                             removeWrapper>
-                            <TableHeader columns={headers}>
+                            <TableHeader columns={headers.filter(header => header.visible)}>
                                 {(header) => (
                                     <TableColumn key={header.uid} className="text-center">
                                         {header.name}
