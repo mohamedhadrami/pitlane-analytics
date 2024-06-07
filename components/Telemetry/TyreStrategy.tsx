8dc66648e537bf6d6c5163472093ffa64580839e
mohamedhadrami <hadrami.mohamed@pringlerobotics.ai>
2024-06-06 20:15:02 -0500

    renamed pitStrategy to tyre strategy

--- components/Telemetry/TyreStrategy.tsx
+++ components/Telemetry/TyreStrategy.tsx
@@ -1,17 +1,19 @@
-// @/components/Telemetry/PitStrategy.tsx
+// @/components/Telemetry/TyreStrategy.tsx
 
 "use client";
 
 import { getCompoundColor } from "@/components/Tyres";
 import { DriverParams, StintParams } from "@/interfaces/openF1";
+import { driverImage } from "@/utils/helpers";
+import { Image } from "@nextui-org/react";
 import React, { useMemo } from 'react';
 
-interface PitStrategyProps {
+interface TyreStrategyProps {
     stints: StintParams[];
     drivers: DriverParams[];
 }
 
-const PitStrategy: React.FC<PitStrategyProps> = ({ stints, drivers }) => {
+const TyreStrategy: React.FC<TyreStrategyProps> = ({ stints, drivers }) => {
     const stintsByDriver = useMemo(() => {
         return stints.reduce((acc, stint) => {
             if (stint.driver_number) {
@@ -27,24 +29,29 @@ const PitStrategy: React.FC<PitStrategyProps> = ({ stints, drivers }) => {
     const driverNameMap = useMemo(() => {
         return drivers.reduce((acc, driver) => {
             if (driver.driver_number) {
-                acc[driver.driver_number] = driver.name_acronym;
+                acc[driver.driver_number] = { acronym: driver.name_acronym, name: driver.full_name, color: `#${driver.team_colour}` };
             }
             return acc;
-        }, {} as { [key: number]: string | undefined });
+        }, {} as { [key: number]: { acronym: string | undefined, name: string | undefined, color: string | undefined } });
     }, [drivers]);
 
     const maxLap = useMemo(() => {
         return Math.max(...stints.map(stint => stint.lap_end || 0));
     }, [stints]);
 
-    console.log(stintsByDriver);
-    console.log(driverNameMap); 
-
     return (
         <div className="max-w-screen-xl mx-auto p-4 w-full">
             {Object.entries(stintsByDriver).map(([driverNumber, driverStints]) => (
-                <div key={driverNumber} className="flex items-center mb-1">
-                    <div className="w-16 text-xl font-semibold">{driverNameMap[parseInt(driverNumber)]}</div>
+                <div key={driverNumber} className="flex items-center my-3">
+                    <Image
+                        className="rounded-full w-7 mr-2"
+                        src={driverImage(driverNameMap[parseInt(driverNumber)].name!)}
+                        alt={`${driverNameMap[parseInt(driverNumber)].name}`} />
+                    <div
+                        className="w-16 text-xl font-semibold"
+                        style={{ color: `${driverNameMap[parseInt(driverNumber)].color}` }}>
+                        {driverNameMap[parseInt(driverNumber)].acronym}
+                    </div>
                     <div className="flex-1 relative bg-[#111] h-8 overflow-hidden rounded-md">
                         {driverStints.map((stint, index) => (
                             <div
@@ -67,4 +74,4 @@ const PitStrategy: React.FC<PitStrategyProps> = ({ stints, drivers }) => {
     );
 };
 
-export default PitStrategy;
+export default TyreStrategy;
