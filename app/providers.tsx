c9c3cb67f59bff5ad92e297286d4cd3b1af5de31
mohamedhadrami <hadrami.mohamed@pringlerobotics.ai>
2024-08-28 00:36:54 -0500

    added SignalR provider to received data throughout the webapp and start service when app is launched

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,14 +2,21 @@
 'use client'
 
 import { FooterProvider } from '@/context/FooterContext'
+import SignalRProvider from '@/context/SignalRProvider'
 import { NextUIProvider } from '@nextui-org/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <FooterProvider>
-        {children}
-      </FooterProvider>
+
+      <SignalRProvider>
+
+        <FooterProvider>
+          {children}
+        </FooterProvider>
+
+      </SignalRProvider>
+
     </NextUIProvider>
   )
 }
