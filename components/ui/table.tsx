35cd8afc04dda7eb02f4e8cb552014ccbc391bd2
mohamedhadrami <mohamedhadrami9@gmail.com>
2025-04-06 23:09:44 -0500

    updated table to show selection

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -58,7 +58,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "transition-colors hover:bg-zinc-100/50 data-[state=selected]:bg-zinc-100 dark:hover:bg-zinc-800/50 dark:data-[state=selected]:bg-zinc-800",
+      "transition-colors hover:bg-zinc-100/50 data-[state=selected]:bg-zinc-100 dark:hover:bg-zinc-800/50 dark:data-[state=selected]:bg-primary/50",
       className
     )}
     {...props}
