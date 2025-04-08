03c177bd234705ac4d18b01d71746367031ea46e
mohamedhadrami <mohamedhadrami9@gmail.com>
2025-04-08 00:16:21 -0500

    Implemented custom tabs

--- components/Telemetry/Views/StatsDriversView.tsx
+++ components/Telemetry/Views/StatsDriversView.tsx
@@ -1,12 +1,14 @@
 // @/components/Telemetry/Views/StatsDriverView.tsx
 
-import { Card, CardBody, Divider, ScrollShadow, Tab, Tabs } from "@heroui/react";
+import { Divider } from "@heroui/react";
 import SessionStats from "../SessionStats";
 import { useTelemetry } from "@/context/Telemetry/TelemetryContext";
 import DriverSelection from "../DriverSelection";
 import TyreStrategy from "../TyreStrategy";
 import LapTimesLineChart from "../LapTimes/LapTimesLineChart";
 import { TelemetryStep } from "@/utils/telemetry/telemetrySteps";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { LifeBuoy, Trophy } from "lucide-react";
 
 
 const StatsDriverView: React.FC = () => {
@@ -26,11 +28,11 @@ const StatsDriverView: React.FC = () => {
                 )}
             </div>
             <div className="flex-grow  border-default/50 overflow-y-auto">
-            {currentStep === TelemetryStep.StatsDrivers ? (
-                <Dash11 />
-            ) : (
-                <Dash12 />
-            )}
+                {currentStep === TelemetryStep.StatsDrivers ? (
+                    <Dash11 />
+                ) : (
+                    <Dash12 />
+                )}
             </div>
         </div>
     );
@@ -50,28 +52,36 @@ const Dash11: React.FC = () => {
     } = useTelemetry();
 
     return (
-        <div className="flex flex-col">
-            <div className="h-1/2">
+        <div className="flex flex-col h-full overflow-y-auto">
+            {/* Top Section */}
+            <div className="flex-shrink-0">
                 {isShowSession && selectedMeeting && selectedSession && weather && (
                     <SessionStats />
                 )}
             </div>
+
             <Divider />
-            <div className="h-1/2 overflow-hidden">
-                <ScrollShadow hideScrollBar className="h-full">
-                    {isShowPitStrategy && stints && drivers && (
-                        <div className="h-full">
-                            <Tabs className="h-full" color="primary">
-                                <Tab key="tyre-strategy" title="Tyre Strategy" className="h-full">
-                                    <TyreStrategy stints={stints} drivers={drivers} />
-                                </Tab>
-                                <Tab key="results" title="Results" className="h-full">
-                                    {/* Content for Results */}
-                                </Tab>
-                            </Tabs>
+
+            {/* Bottom Section: Tabs */}
+            <div className="flex-1 overflow-hidden">
+                {isShowPitStrategy && stints && drivers && (
+                    <Tabs defaultValue="tyre-strategy" className="flex flex-col h-full">
+                        <TabsList>
+                            <TabsTrigger value="tyre-strategy" className="gap-2"><LifeBuoy />Tyre Strategy</TabsTrigger>
+                            <TabsTrigger value="results" className="gap-2"><Trophy />Results</TabsTrigger>
+                        </TabsList>
+
+                        {/* Scrollable Content */}
+                        <div className="flex-1 overflow-y-auto">
+                            <TabsContent value="tyre-strategy" className="p-4">
+                                <TyreStrategy stints={stints} drivers={drivers} />
+                            </TabsContent>
+                            <TabsContent value="results" className="p-4">
+                                {/* Results content here */}
+                            </TabsContent>
                         </div>
-                    )}
-                </ScrollShadow>
+                    </Tabs>
+                )}
             </div>
         </div>
     );
